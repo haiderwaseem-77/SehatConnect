b6ae9c232c1cb52acb0d6a2026584ddc2e8d90a3
haiderwaseem-77 <haiderwaseem9999@gmail.com>
2026-07-06 23:14:36 -0400

    Update final CTA subtext

--- components/home/CtaBanner.tsx
+++ components/home/CtaBanner.tsx
@@ -14,8 +14,8 @@ export default function CtaBanner() {
               <span data-ur className="urdu">بس اپنا نمبر دیں۔ ہم آپ کو کال کریں گے۔</span>
             </h2>
             <p className="c-sub">
-              <span data-en>Name and phone only. We&rsquo;ll call, understand the situation, and arrange the right nurse or attendant.</span>
-              <span data-ur className="urdu">صرف نام اور فون۔ ہم کال کر کے صورتحال سمجھیں گے اور صحیح نرس یا اٹینڈنٹ کا بندوبست کریں گے۔</span>
+              <span data-en>It takes less than 30 seconds. We&rsquo;ll help arrange the care your loved one needs.</span>
+              <span data-ur className="urdu">30 سیکنڈ سے کم وقت لگتا ہے۔ ہم آپ کے پیارے کے لیے ضروری دیکھ بھال کا بندوبست کرنے میں مدد کریں گے۔</span>
             </p>
             <p className="closer-urdu">
               <span data-en>Call or WhatsApp. Our team answers.</span>
